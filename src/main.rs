8f1988c68fd5e2ba3c5174e3dac7cb1f290589f0
Zach Petch <zachpetch@gmail.com>
2024-02-27 08:21:01 -0800

    using unstable and nightly

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,5 @@
-#![no_std]
-#![no_main]
+#![no_std]  // Don't link the Rust standard library
+#![no_main] // Disable all Rust-level entry points
 
 use core::panic::PanicInfo;
 
@@ -9,6 +9,9 @@ fn panic(_info: &PanicInfo) -> ! {
     loop {}
 }
 
+/// This function is the entry point.
+/// The linker looks for a function named `_start` by default, which is why the
+/// `no_mangle` tag is used (so the compiler won't touch/"mangle" the name).
 #[no_mangle]
 pub extern "C" fn _start() -> ! {
     loop {}
